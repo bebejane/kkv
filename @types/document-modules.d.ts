7fd9184d08c4dafab1d211b5d03446eb0996c95f
Bebe Jane <bjornthief@gmail.com>
2025-04-12 13:00:51 +0200

    sitemap

--- @types/document-modules.d.ts
+++ @types/document-modules.d.ts
@@ -135,6 +135,15 @@ declare module '*/seo.gql' {
 }
     
 
+declare module '*/sitemap.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Sitemap: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
 declare module '*/start.gql' {
   import { DocumentNode } from 'graphql';
   const defaultDocument: DocumentNode;
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,42 @@
 import { MetadataRoute } from 'next'
+import { apiQuery } from 'next-dato-utils/api'
+import { SitemapDocument } from '@/graphql'
 
 const routes = ['/', '/kurser', '/verkstader', '/kontakt', '/om', '/aktuellt']
 
-const staticRoutes: MetadataRoute.Sitemap = [
+const staticRoutes = [
   ...routes.map((route) => ({
     url: `${process.env.NEXT_PUBLIC_SITE_URL}${route}`,
     lastModified: new Date(),
     changeFrequency: 'daily',
     priority: 1,
-  }))]
+  }))
+]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-  return [
-    ...staticRoutes
+  const { allAbouts, allWorkshops, allCourses, allNews } = await apiQuery<SitemapQuery, SitemapQueryVariables>(
+    SitemapDocument,
+    {
+      variables: {
+        first: 100,
+        skip: 0,
+      },
+      all: true,
+    }
+  )
+
+  const abouts = allAbouts.map(({ slug, _publishedAt }) => ({ url: `${process.env.NEXT_PUBLIC_SITE_URL}/om/${slug}`, lastModified: new Date(_publishedAt), changeFrequency: 'monthly', priority: 0.9 }))
+  const workshops = allWorkshops.map(({ slug, _publishedAt }) => ({ url: `${process.env.NEXT_PUBLIC_SITE_URL}/verkstader/${slug}`, lastModified: new Date(_publishedAt), changeFrequency: 'monthly', priority: 0.9 }))
+  const courses = allCourses.map(({ slug, _publishedAt }) => ({ url: `${process.env.NEXT_PUBLIC_SITE_URL}/kurser/${slug}`, lastModified: new Date(_publishedAt), changeFrequency: 'weekly', priority: 0.9 }))
+  const news = allNews.map(({ slug, _publishedAt }) => ({ url: `${process.env.NEXT_PUBLIC_SITE_URL}/aktuellt/${slug}`, lastModified: new Date(_publishedAt), changeFrequency: 'daily', priority: 0.9 }))
+  const sitemap = [
+    ...staticRoutes,
+    ...abouts,
+    ...workshops,
+    ...courses,
+    ...news,
   ]
+
+  return sitemap as MetadataRoute.Sitemap
 }
